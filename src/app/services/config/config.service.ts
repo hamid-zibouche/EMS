de78db125adbd5b75aba9b04ea22c089aee261ab
hamid zibouche <hzibouche@go-easier.com>
2024-06-17 14:29:13 +0200

    version 1.1.2

--- src/app/services/config/config.service.ts
+++ src/app/services/config/config.service.ts
@@ -5,12 +5,12 @@ import {HttpClient, HttpErrorResponse} from "@angular/common/http";
   providedIn: 'root'
 })
 export class ConfigService {
-  private readonly CONFIG_URL = '/assets/config/config.json';
+  private readonly CONFIG_URL = '/ems-home/assets/config/config.json';
   private config: any;
 
   constructor(private http: HttpClient) {}
 
   getConfig() {
-    return this.http.get('/assets/config/config.json');
+    return this.http.get('/ems-home/assets/config/config.json');
   }
 }
